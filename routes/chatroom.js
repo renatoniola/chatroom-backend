1ad539f2f3cfd5b6e6f1bfc17bd0c6666f275a2f
renato niola <niolarenato01@gmail.com>
2018-04-01 16:58:25 +0200

    add socket io

--- routes/chatroom.js
+++ routes/chatroom.js
@@ -5,26 +5,29 @@ var {Chatroom} = require('./../models/chatroom');
 const {authenticate} = require('../middleware/authenticate');
 
 router.post('/chatroom',authenticate, (req, res) => {
-    
+
     Chatroom.find({ users: { "$in" : [req.body.targetuser] } , users: { "$in" : [req.user._id] } }, (err, chatroom) => {
        if(err){ res.send(err); }
-       
+
 
        if( chatroom.length > 0 ){
 
-           
+
 
            let messages = chatroom[0].messages;
-           
+
            messages.push({
             text : req.body.message,
             _creator : req.user._id
            });
 
            chatroom[0].messages = messages;
-           
+
            chatroom[0].save(function (err,chatroom) {
             if (err) return res.status(401).send(err);
+
+            req.io.sockets.emit('MESSAGE_SENT',chatroom);
+
             res.status(200).send(chatroom)
           })
 
@@ -44,16 +47,16 @@ router.post('/chatroom',authenticate, (req, res) => {
                users : users,
                messages : message
 
-           }) 
+           })
            chatroom.save(function (err) {
             if (err) return res.status(401).send(err);
             res.status(200).send(chatroom)
-            
+
           })
        }
     });
 
-    
+
   });
 
   router.get('/chatrooms',authenticate, (req, res) => {
